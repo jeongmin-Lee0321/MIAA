2bcd4e9b8ffd1256f6f403152c8e67064d5c4df0
weonjinho <yuanzhenhao59@gmail.com>
2024-04-01 17:25:17 +0900

    weon_0401_pm

---
+++ src/main/java/com/tech/miaa/controller/MypageMatchingAlarmController.java
@@ -0,0 +1,28 @@
+package com.tech.miaa.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.tech.miaa.serviceInter.MypageMatchingAlarmServiceInter;
+//원진호_알림목록_0401추가
+@Controller
+public class MypageMatchingAlarmController {
+	@Autowired
+	private SqlSession sqlSession;
+	MypageMatchingAlarmServiceInter mypageMatchingAlarmServiceInter;
+	
+	/* 원진호_0401추가 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String mypage_matching_alarm_list_page(HttpServletRequest request, Model model, @SessionAttribute(name = "userId", required = false) String userId) {
+		return "mypage_matching_alarm.list.알림 목록.2";
+	}
+	
+	
+}
