85a9266fc27e22190667c19f09e5bf0e923576a6
kimgeen <boy25860@naver.com>
2024-04-01 16:09:46 +0900

    파일 다중업로드 중

--- src/main/java/com/tech/miaa/dao/ItemDao.java
+++ src/main/java/com/tech/miaa/dao/ItemDao.java
@@ -7,7 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.tech.miaa.dto.ItemDto;
 
 public interface ItemDao {
+	public void imgUpLoad(String userId,String itemName, String fileName);
 	public void itemWrite(String tel,String openclose, String lostday,String address,String itemname,
-			String itemkind1,String itemkind2,String colorCd,String sepcialMark,String userId, MultipartFile file);
+			String itemkind1,String itemkind2,String colorCd,String sepcialMark,String userId);
 	public ArrayList<ItemDto> itemlistview();
 }
