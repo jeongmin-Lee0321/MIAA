49a6a8b300d22ba798e3b3dee28857872244ebc2
MSW <claja@naver.com>
2024-04-04 19:49:08 +0900

    게시글수정추가

--- src/main/java/com/tech/miaa/dao/InquiryDao.java
+++ src/main/java/com/tech/miaa/dao/InquiryDao.java
@@ -9,8 +9,14 @@ public interface InquiryDao {
 	public void inquiry_wirte(String id, String title, String content, String file);
 	
 	public ArrayList<InquiryDto> inquiry_list(String id);
+	
+	public InquiryDto modify_list(String board_num, String id);
 
 	public void delete(String string, String id);
+
+	public void inquiry_modify(String num, String id, String title, String content, String file);
+
+	public void inquiry_modify_present(String num, String id, String title, String content, String file_hidden);
 
 	
 	
--- src/main/java/com/tech/miaa/service/InquiryService.java
+++ src/main/java/com/tech/miaa/service/InquiryService.java
@@ -40,7 +40,6 @@ public class InquiryService implements MypageCustomerInquiryServiceInter {
 		SqlSession sqlSession = (SqlSession) map.get("sqlSession");
 		HttpSession session=request.getSession();
 		
-		String board_num=request.getParameter("board_num");
 		String id = (String) session.getAttribute("userId");
 		
 		InquiryDao dao = sqlSession.getMapper(InquiryDao.class);
@@ -69,5 +68,52 @@ public class InquiryService implements MypageCustomerInquiryServiceInter {
 		dao.delete(string,id);
 		
 	}
+	@Override
+	public InquiryDto modify_list(Model model) {
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		SqlSession sqlSession = (SqlSession) map.get("sqlSession");
+		HttpSession session=request.getSession();
+		
+		String board_num=request.getParameter("board_num");
+		String id = (String) session.getAttribute("userId");
+		
+		InquiryDao dao = sqlSession.getMapper(InquiryDao.class);
+		InquiryDto list=null;
+		try {
+			list = dao.modify_list(board_num,id);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+	@Override
+	public void inquiry_modify(Model model) {
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		SqlSession sqlSession = (SqlSession) map.get("sqlSession");
+		HttpSession session=request.getSession();
+		
+	
+		String num = request.getParameter("inquiry_num");
+		String id = (String) session.getAttribute("userId");
+		String title=request.getParameter("inquiry_title");
+		String content=request.getParameter("inquiry_content");
+		String file=request.getParameter("inquiry_file");
+		String file_hidden=request.getParameter("inquiry_file_hidden");
+		System.out.println("file : "+file);
+		System.out.println("filehidden : "+file_hidden);
+		
+		InquiryDao dao = sqlSession.getMapper(InquiryDao.class);
+		if(file_hidden!=null && file==null) {	
+			dao.inquiry_modify_present(num,id,title,content,file_hidden);
+		}
+		else {
+			dao.inquiry_modify(num,id,title,content,file);
+		}
+		
 
 }
+	}
