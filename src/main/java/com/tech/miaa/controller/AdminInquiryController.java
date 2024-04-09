6b56f947d8bc42dc6f03ab99108c000b48d1439d
Ybuddle <kyb0411@naver.com>
2024-04-09 15:28:43 +0900

    1대1 문의 sql 매퍼 작성 , 회원 1대1문의 select 문 및 관리자테이블 글번호입력 메서드 추가 및 변경

--- src/main/java/com/tech/miaa/controller/AdminInquiryController.java
+++ src/main/java/com/tech/miaa/controller/AdminInquiryController.java
@@ -11,28 +11,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
-import com.tech.miaa.dto.InquiryDto;
-import com.tech.miaa.service.InquiryService;
-import com.tech.miaa.serviceInter.MypageCustomerInquiryServiceInter;
+import com.tech.miaa.dto.AdminInquiryDto;
+import com.tech.miaa.service.AdminInquiryService;
+import com.tech.miaa.serviceInter.AdminInquiryServiceInter;
+import com.tech.miaa.vopage.PageVO;
 
 @Controller
 public class AdminInquiryController {
 	@Autowired
 	private SqlSession sqlSession;
 
-	MypageCustomerInquiryServiceInter mypageCustomerInquiryServiceInter;
+	AdminInquiryServiceInter adminInquiryInter;
 	
 
 //	문의내역 페이지
 	@RequestMapping("admin_inquiry_list_page")
 	public String admin_inquiry_list_page(HttpServletRequest request, Model model, @SessionAttribute(name = "userId", required = false) String userId,
 			@SessionAttribute(name = "isAdmin", required = false) String isAdmin) {
-
 		String result = "redirect:/";
-		if (isAdmin == null) {
-			
-			System.out.println("관리자아이디가 아닙니다");
+		String strPage = null;
+		
+		//접속자가 관리자인지 확인 후 뷰단경로 처리
+		if (isAdmin == null) {		
 			if (userId != null) {
+				System.out.println("관리자아이디가 아닙니다");
 				System.out.println("로그인 유저의 id : " + userId);
 			} else if (userId == null) {
 				System.out.println("로그인 하지 않았습니다.");
@@ -40,22 +42,37 @@ public class AdminInquiryController {
 		}
 		else if (isAdmin.equals("admin")) {
 			System.out.println("관리자입니다.");
-			result = "admin_inquiry.list_page.1대1문의관리.3a";
-			
-			model.addAttribute("userId", userId);
-			model.addAttribute("IsAdmin", isAdmin);
-			model.addAttribute("request", request);
-			model.addAttribute("sqlSession", sqlSession);
-			
-			try {
-				mypageCustomerInquiryServiceInter=new InquiryService();
-				ArrayList<InquiryDto> list=mypageCustomerInquiryServiceInter.inquiry_list(model);
-				model.addAttribute("list",list);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			result = "admin_inquiry.list_page.1대1문의관리.3a";	
 		}
+		
+		//싱글톤위한 값 전달
+		model.addAttribute("userId", userId);
+		model.addAttribute("IsAdmin", isAdmin);
+		model.addAttribute("request", request);
+		model.addAttribute("sqlSession", sqlSession);
+		
+		//inquiry 조인 테이블 가져오와서 모델에 넘기기
+		adminInquiryInter=new AdminInquiryService();
+		try {
+			ArrayList<AdminInquiryDto> list=adminInquiryInter.inquiry_list(model);
+			model.addAttribute("list",list);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//페이징 처리
+			//시작페이지 전달
+		try {
+			strPage = request.getParameter("strPage");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		model.addAttribute("strPage", strPage);
+		
+		PageVO pageVo = null;
+		pageVo = adminInquiryInter.get_pagevo(model);
 		
 			
 		return result;
--- src/main/java/com/tech/miaa/dao/InquiryDao.java
+++ src/main/java/com/tech/miaa/dao/InquiryDao.java
@@ -1,18 +1,19 @@
 package com.tech.miaa.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.tech.miaa.dto.InquiryDto;
 
 public interface InquiryDao {
 
-	public void inquiry_wirte(String id, String title, String content, String file);
+	public void inquiry_wirte(HashMap<String,Object> map1);
 	
 	public ArrayList<InquiryDto> inquiry_list(String id);
 
 	public void delete(String string, String id);
 
-	
+	public void admin_inquiry_add(HashMap<String,Object> map1);
 	
 	
 }
