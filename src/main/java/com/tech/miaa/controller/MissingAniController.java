3d3b4bc97d113e885013ad812900cfdfe7b079ca
kimgeen <boy25860@naver.com>
2024-04-15 09:53:03 +0900

    디벨롭 풀

--- src/main/java/com/tech/miaa/controller/MissingAniController.java
+++ src/main/java/com/tech/miaa/controller/MissingAniController.java
@@ -2,8 +2,6 @@ package com.tech.miaa.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.tech.miaa.abdmApi.AbandonmentPublicSrvc;
-import com.tech.miaa.abdmApi.AbdmSido;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
