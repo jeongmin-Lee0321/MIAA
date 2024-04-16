d62e391caac36570fcff997dcc5a448fb2552684
kimgeen <boy25860@naver.com>
2024-04-16 09:43:18 +0900

    충돌 제거

--- src/main/java/com/tech/miaa/dao/LostItemDao.java
+++ src/main/java/com/tech/miaa/dao/LostItemDao.java
@@ -22,11 +22,8 @@ public interface LostItemDao {
 	public void lost_item_delete_content(String total_id);
 	public void lost_item_modify(String tel, String openclose, String lostday, String address, String itemname,
 			String upkind, String upr_cd, String colorCd, String sepcialMark, String addressCode, String total_id);
-<<<<<<< HEAD
 	public ArrayList<ItemDto> get_lost_items(String userId);
-=======
 	public ArrayList<ItemDto> itemlistview(@Param("searchday1")String searchday1, @Param("searchday2")String searchday2, 
 			@Param("addressCode") String addressCode, @Param("itemkind1")String itemkind1, @Param("itemkind2")String itemkind2, 
 			@Param("colorCd")String colorCd, @Param("rowStart") int rowStart, @Param("rowEnd") int rowEnd);
->>>>>>> release/KGY
 }
