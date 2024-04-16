cb03f5df42ff689cff447b29805962558e9ce68f
negaihoshinano <negaihoshinano@gmail.com>
2024-04-16 14:55:24 +0900

    scheduler 분실문 1차완료

--- src/main/java/com/tech/miaa/scheduler/Scheduler.java
+++ src/main/java/com/tech/miaa/scheduler/Scheduler.java
@@ -43,17 +43,7 @@ public class Scheduler {
 //            //분실물
             LostItemDao lostItemDao = sqlSession.getMapper(LostItemDao.class);
             List<ItemDto> itemDtos =lostItemDao.get_lost_items(memeberId);
-            PageVO pageVO = new PageVO();
             for (int j = 0; j <itemDtos.size(); j++){
-//                System.out.println(itemDtos.get(j).getTotal_id());
-//                System.out.println(itemDtos.get(j).getItem_id());
-//                System.out.println(itemDtos.get(j).getUser_tel());
-//                System.out.println(itemDtos.get(j).getUpkind());
-//                System.out.println(itemDtos.get(j).getUpr_cd());
-//                System.out.println(itemDtos.get(j).getColorcd());
-//                System.out.println(itemDtos.get(j).getLostday());
-//                System.out.println(itemDtos.get(j).getAddressCode());
-
                 //api 호출
                 FounditemService founditemService = new FounditemService();
                 String a = founditemService.found_item_search_AreaPd(itemDtos.get(i));
@@ -63,9 +53,52 @@ public class Scheduler {
                 //db 저장
                 MatchingAlarmDao matchingAlarmDao = sqlSession.getMapper(MatchingAlarmDao.class);
                 for (int t = 0; t<founditemDtos.size(); t++){
+//                    System.out.println(memeberId);
+//                    System.out.println(founditemDtos.get(t).getAtcid());
+//                    System.out.println(founditemDtos.get(t).getFdSbjt());
+//                    System.out.println(founditemDtos.get(t).getFdSn());
+//                    System.out.println(founditemDtos.get(t).getPrdtClNm());
+//                    System.out.println(founditemDtos.get(t).getFdPrdtNm());
+//                    System.out.println(founditemDtos.get(t).getFdYmd());
+//                    System.out.println(founditemDtos.get(t).getDepPlace());
+//                    System.out.println(founditemDtos.get(t).getRnum());
+//                    System.out.println(founditemDtos.get(t).getClrNm());
+//                    System.out.println(founditemDtos.get(t).getFdFilePathImg());
+//                    System.out.println(founditemDtos.get(t).getAddr());
+                    if(founditemDtos.get(t).getAtcid() == null){
+                        founditemDtos.get(t).setAtcid("");
+                    }
+                    if(founditemDtos.get(t).getFdSbjt() == null){
+                        founditemDtos.get(t).setFdSbjt("");
+                    }
+                    if(founditemDtos.get(t).getFdSn() == null){
+                        founditemDtos.get(t).setFdSn("");
+                    }
+                    if(founditemDtos.get(t).getPrdtClNm() == null){
+                        founditemDtos.get(t).setPrdtClNm("");
+                    }
+                    if(founditemDtos.get(t).getFdPrdtNm() == null){
+                        founditemDtos.get(t).setFdPrdtNm("");
+                    }
+                    if(founditemDtos.get(t).getFdYmd() == null){
+                        founditemDtos.get(t).setFdYmd("");
+                    }
+                    if(founditemDtos.get(t).getDepPlace() == null){
+                        founditemDtos.get(t).setDepPlace("");
+                    }
+                    if(founditemDtos.get(t).getRnum() == null){
+                        founditemDtos.get(t).setRnum("");
+                    }
+                    if(founditemDtos.get(t).getClrNm() == null){
+                        founditemDtos.get(t).setClrNm("");
+                    }
+                    if(founditemDtos.get(t).getFdFilePathImg() == null){
+                        founditemDtos.get(t).setFdFilePathImg("");
+                    }
+                    if(founditemDtos.get(t).getAddr() == null){
+                        founditemDtos.get(t).setAddr("");
+                    }
 
-                    System.out.println(memeberId);
-                    System.out.println(founditemDtos.get(t).getAtcid());
                     matchingAlarmDao.alert_item_write(memeberId,founditemDtos.get(t).getAtcid(),
                             founditemDtos.get(t).getFdSbjt(),founditemDtos.get(t).getFdSn(),founditemDtos.get(t).getPrdtClNm(),
                             founditemDtos.get(t).getFdPrdtNm(),founditemDtos.get(t).getFdYmd(),founditemDtos.get(t).getDepPlace(),
@@ -75,7 +108,6 @@ public class Scheduler {
 
             }
 //            //유기동물
-
 
 
         }
