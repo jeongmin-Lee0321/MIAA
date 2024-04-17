a1ca86a8b32e7e1bc2b388d0b59b10948cf3ef79
weonjinho <yuanzhenhao59@gmail.com>
2024-04-17 15:17:28 +0900

    원진호_0417_pm_commit

--- src/main/java/com/tech/miaa/dao/MatchingAlarmDao.java
+++ src/main/java/com/tech/miaa/dao/MatchingAlarmDao.java
@@ -2,11 +2,20 @@ package com.tech.miaa.dao;
 
 import java.util.ArrayList;
 
+import com.tech.miaa.dto.FounditemDto;
 import com.tech.miaa.dto.ItemDto;
+import com.tech.miaa.dto.matchingAlarmDto;
+
+import org.apache.ibatis.annotations.Param;
 
 /* 원진호_0403_matching_alarm_list(String id)추가 */
 public interface MatchingAlarmDao {
    public ArrayList<ItemDto> matching_alarm_list(String id);
    /*원진호_0412_원글삭제기능추가*/
-	public void mypage_alarm_delete(String string, String id);
+   public void mypage_alarm_delete(String string, String id);
+   public void alert_item_write(String total_id,String user_id,String atcid,String fdSbjt,String fdSn,
+                                 String prdtClNm,String fdPrdtNm,String fdYmd,String depPlace,
+                                 String rnum,String clrNm,String fdFilePathImg,String addr);
+   public ArrayList<matchingAlarmDto> alert_item_list(String id);
+
 }
