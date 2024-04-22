560c196147d0dabd4c7a3062adaa6aa6cbc3d0f8
Ybuddle <kyb0411@naver.com>
2024-04-22 19:29:29 +0900

    오류 코드 우선 수정

--- src/main/java/com/tech/miaa/serviceInter/AdminMainDataServiceInter.java
+++ src/main/java/com/tech/miaa/serviceInter/AdminMainDataServiceInter.java
@@ -1,9 +1,6 @@
 package com.tech.miaa.serviceInter;
 
 import org.springframework.ui.Model;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.tech.miaa.vopage.PageVO;
 
 public interface AdminMainDataServiceInter {
 	public void get_main_data(Model model);
